8fec8012acd6533f3f94ad148b851baa5dac5397
p-nanaware <pnanaware.umassd.edu>
2024-11-04 15:36:17 -0800

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { HeroParticle } from "@/components/hero/heroParticles";
+import GradualSpacing from "@/components/ui/gradual-spacing";
+import Image from "next/image";
+
+import BlurIn from "@/components/ui/blur-in";
+import { Mail } from "lucide-react";
+
+export default function Home() {
+  return (
+    <div className="relative">
+      <div className="absolute top-0 w-full h-screen z-10 flex items-center justify-center flex-col space-y-10">
+        <div className="flex flex-col items-center justify-center ">
+          {" "}
+          <Image src={"/pellican.png"} alt={""} height={200} width={200} />
+          <GradualSpacing
+            className="font-display text-center text-4xl font-bold -tracking-widest  text-black dark:text-white md:text-7xl md:leading-[5rem]"
+            text="A global entertainment "
+          />
+          <GradualSpacing
+            className="font-display text-center text-4xl font-bold -tracking-widest  text-black dark:text-white md:text-7xl md:leading-[5rem]"
+            text="and technology company"
+          />
+        </div>
+
+        <div className="flex flex-col space-y-2">
+          <BlurIn
+            word="2108 N ST #9219 Sacramento, CA 95816"
+            className="text-xl font-medium text-black"
+          />
+          <a
+            href="mailto:jp@pelicanholdings.com"
+            className="text-xl font-medium text-black"
+          >
+            <BlurIn word="Email: jp@pelicanholdings.com" />
+          </a>
+        </div>
+      </div>
+
+      <HeroParticle />
+    </div>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="fixed top-0 w-full">
+      <div className="max-w-7xl h-16 justify-between">
+        <h1>Efficient</h1>
+        <h1>Efficient Studios.</h1>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
